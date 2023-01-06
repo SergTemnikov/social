f150fd06dec4dc29ba43bce24bb3e82ea9265970
Sergey Temnikov <temnikov.s.vik@yandex.ru>
2023-01-07 02:31:13 +0300

    Fine changes in App.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from "react"
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import store from './redux/redux-store'
+import store from './redux/index'
 import App from "./components/App/App"
 import { Provider } from "react-redux"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-let reRenderEntireTree = (state) => {
-  root.render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <App state={state} dispatch={store.dispatch.bind(store)} />
-      </BrowserRouter>
-    </Provider>
-  )
-}
-
-reRenderEntireTree(store.getState())
-
-store.subscribe(reRenderEntireTree)
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+)
